e7bc82eb277e54ad5fbb75c2327c185af92ef076
Limekiller <me@bryceyoder.com>
2022-01-06 16:14:59 -0500

    Remove settings file and mark stable

--- version.php
+++ version.php
@@ -25,5 +25,5 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'local_checkboxes';
 $plugin->version = 2022010600;
 $plugin->requires = 2021051700;
-$plugin->maturity = MATURITY_ALPHA;
+$plugin->maturity = MATURITY_STABLE;
 $plugin->release = 'v1.0';
